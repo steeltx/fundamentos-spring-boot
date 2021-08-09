3e4947ee26f796cab7f4bf9cd7c081644d4db7fa
steeltx <realsteel.gnu@gmail.com>
2021-08-08 20:31:32 -0500

    Inyección de dependencia con component

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentImplement.java
@@ -0,0 +1,12 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentImplement implements ComponentDependency{
+
+    @Override
+    public void saludar() {
+        System.out.println("Hola mundo desde mi componente");
+    }
+}
