c14a840e7604c9c509542cd58c731d1828021f04
steeltx <realsteel.gnu@gmail.com>
2021-08-08 23:18:42 -0500

    Usando properties con pojo

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,8 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,15 +18,18 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
 	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 								  MyBean myBean,
 								  MyBeanWithDependency myBeanWithDependency,
-								  MyBeanWithProperties myBeanWithProperties){
+								  MyBeanWithProperties myBeanWithProperties,
+								  UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 
 	public static void main(String[] args) {
@@ -37,5 +42,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDepedency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail()+" - "+userPojo.getPassword());
 	}
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.fundamentosplatzi.springboot.fundamentos.configuration;
 
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
 
     // obtener los valores del archivo properties
