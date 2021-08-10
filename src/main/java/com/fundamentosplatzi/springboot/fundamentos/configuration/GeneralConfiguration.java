c1ed8c5e2f10003b72d05b01cfd3b09a55ff6373
steeltx <realsteel.gnu@gmail.com>
2021-08-10 00:22:48 -0500

    Configuración de datasource

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImp
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -25,6 +28,17 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(nombre,apellido);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+
+        return dataSourceBuilder.build();
     }
 
 }
