c5a83f9f099975b844865e45842045700df6acf3
steeltx <realsteel.gnu@gmail.com>
2021-08-14 18:50:10 -0500

    Implementando paginación

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -64,7 +64,7 @@ public class FundamentosApplication implements CommandLineRunner {
 	private void saveWithErrorTransactional(){
 		User user1 = new User ("PruebaTransactional1","pruebaTransactional1@prueba.com", LocalDate.now());
 		User user2 = new User ("PruebaTransactional2","pruebaTransactional2@prueba.com", LocalDate.now());
-		User user3 = new User ("PruebaTransactional3","pruebaTransactional1@prueba.com", LocalDate.now());
+		User user3 = new User ("PruebaTransactional3","pruebaTransactional3@prueba.com", LocalDate.now());
 		User user4 = new User ("PruebaTransactional4","pruebaTransactional4@prueba.com", LocalDate.now());
 
 		List<User> users = Arrays.asList(user1, user2, user3, user4);
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/controller/UserRestController.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/controller/UserRestController.java
@@ -5,6 +5,8 @@ import com.fundamentosplatzi.springboot.fundamentos.caseuse.DeleteUser;
 import com.fundamentosplatzi.springboot.fundamentos.caseuse.GetUser;
 import com.fundamentosplatzi.springboot.fundamentos.caseuse.UpdateUser;
 import com.fundamentosplatzi.springboot.fundamentos.entity.User;
+import com.fundamentosplatzi.springboot.fundamentos.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +22,14 @@ public class UserRestController {
     private CreateUser createUser;
     private DeleteUser deleteUser;
     private UpdateUser updateUser;
+    private UserRepository userRepository;
 
-    public UserRestController(GetUser getUser, CreateUser createUser,DeleteUser deleteUser,UpdateUser updateUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser,DeleteUser deleteUser,UpdateUser updateUser, UserRepository userRepository) {
         this.getUser = getUser;
         this.createUser = createUser;
         this.deleteUser = deleteUser;
         this.updateUser = updateUser;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -47,6 +51,11 @@ public class UserRestController {
     @PutMapping("/{id}")
     ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
         return new ResponseEntity(updateUser.update(newUser, id),HttpStatus.OK);
+    }
+
+    @GetMapping("/pageable")
+    List<User> getUserPageable(@RequestParam int page, @RequestParam int size){
+        return userRepository.findAll(PageRequest.of(page, size)).getContent();
     }
 
 }
