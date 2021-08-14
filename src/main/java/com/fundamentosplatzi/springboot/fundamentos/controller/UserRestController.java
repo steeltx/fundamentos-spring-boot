5c1c4f69ba16d9bb46a5630f38e260186dc511c5
steeltx <realsteel.gnu@gmail.com>
2021-08-14 11:26:12 -0500

    Creación de métodos CRUD

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/controller/UserRestController.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/controller/UserRestController.java
@@ -1,10 +1,13 @@
 package com.fundamentosplatzi.springboot.fundamentos.controller;
 
+import com.fundamentosplatzi.springboot.fundamentos.caseuse.CreateUser;
+import com.fundamentosplatzi.springboot.fundamentos.caseuse.DeleteUser;
 import com.fundamentosplatzi.springboot.fundamentos.caseuse.GetUser;
+import com.fundamentosplatzi.springboot.fundamentos.caseuse.UpdateUser;
 import com.fundamentosplatzi.springboot.fundamentos.entity.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,13 +17,36 @@ public class UserRestController {
 
     // crear, obtener, eliminar, actualizar
     private GetUser getUser;
+    private CreateUser createUser;
+    private DeleteUser deleteUser;
+    private UpdateUser updateUser;
 
-    public UserRestController(GetUser getUser) {
+    public UserRestController(GetUser getUser, CreateUser createUser,DeleteUser deleteUser,UpdateUser updateUser) {
         this.getUser = getUser;
+        this.createUser = createUser;
+        this.deleteUser = deleteUser;
+        this.updateUser = updateUser;
     }
 
     @GetMapping("/")
     List<User> get(){
         return getUser.getAll();
     }
+
+    @PostMapping("/")
+    ResponseEntity<User> newUser(@RequestBody User newUser){
+        return new ResponseEntity<>(createUser.save(newUser), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity deleteUser(@PathVariable Long id){
+        deleteUser.remove(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<User> replaceUser(@RequestBody User newUser, @PathVariable Long id){
+        return new ResponseEntity(updateUser.update(newUser, id),HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/User.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/User.java
@@ -37,6 +37,10 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    public User(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
